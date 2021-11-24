01edb14401835ac61f00f9d01e0e09e8f4020c48
Gerardo Rodrigues <gerardofilho56@gmail.com>
2021-11-24 11:48:24 -0300

    Consulta simple com query methods

--- src/main/java/com/example/course/resource/PostResource.java
+++ src/main/java/com/example/course/resource/PostResource.java
@@ -7,9 +7,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.course.domain.Post;
+import com.example.course.resource.util.URL;
 import com.example.course.services.PostService;
 
 @RestController
@@ -29,5 +31,12 @@ public class PostResource {
 	public ResponseEntity<Post> findById(@PathVariable String id){
 		Post obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@GetMapping(value = "/titlesearch")
+	public ResponseEntity<List<Post>> findByTile(@RequestParam(value = "text", defaultValue = "") String text){
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
 	}
 }
